8db8ff4ff3add6f5ef8c206c9fe96b29450877f7
satkar2001 <satkaracharya883@gmail.com>
2024-05-28 23:25:33 +0530

    changes in UI

---
+++ app/component/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-[#1e293b] text-white py-4 px-6 shadow-md">
+      <h1 className="text-2xl font-bold">IntelliChat</h1>
+    </header>
+  );
+};
+
+export default Header;
